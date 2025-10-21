5162f90491d6609370baac6afe67c520b4fa3cbf
gbyn <byank2314@gmail.com>
2025-10-21 15:25:22 +0700

    jobsheet7

--- jobsheet7/SiakadFor18.java
+++ jobsheet7/SiakadFor18.java
@@ -5,15 +5,13 @@ public class SiakadFor18 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
-        int i = 1;
-        while (i <= 10) {
+        for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = input.nextDouble();
             if (nilai > tertinggi)
                 tertinggi = nilai;
             if (nilai < terendah)
                 terendah = nilai;
-            i++;
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
