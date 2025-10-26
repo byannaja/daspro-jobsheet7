9858fa53152cd5890b3c8c4f16f2994084a8468b
gbyn <byank2314@gmail.com>
2025-10-26 19:14:25 +0700

    latihan2

---
+++ jobsheet7/SiakadWhile18.java
@@ -0,0 +1,43 @@
+package jobsheet7;
+import java.util.Scanner;
+
+public class SiakadWhile18 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jml, i = 0;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jml = sc.nextInt();
+
+        while (i < jml) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            int nilai = sc.nextInt();
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai tidak valid! Masukkan ulang.");
+                continue;
+            }
+
+            if (nilai > 80) {
+                System.out.println("Nilai huruf: A");
+            } else if (nilai > 73) {
+                System.out.println("Nilai huruf: B+");
+            } else if (nilai > 65) {
+                System.out.println("Nilai huruf: B");
+            } else if (nilai > 60) {
+                System.out.println("Nilai huruf: C+");
+            } else if (nilai > 50) {
+                System.out.println("Nilai huruf: C");
+            } else if (nilai > 39) {
+                System.out.println("Nilai huruf: D");
+            } else {
+                System.out.println("Nilai huruf: E");
+            }
+
+            i++;
+        }
+
+        sc.close();
+    }
+}
