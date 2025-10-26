aba265c63189d169e5b4d3b416df378291cf8a11
gbyn <byank2314@gmail.com>
2025-10-26 19:28:51 +0700

    latihan1 pembaruan

--- jobsheet7/SiakadFor18.java
+++ jobsheet7/SiakadFor18.java
@@ -3,18 +3,38 @@ import java.util.Scanner;
 
 public class SiakadFor18 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
+
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
-            nilai = input.nextDouble();
-            if (nilai > tertinggi)
-                tertinggi = nilai;
-            if (nilai < terendah)
-                terendah = nilai;
-                //
+
+           
+            String input = sc.nextLine();
+
+            try {
+                nilai = Double.parseDouble(input);
+
+                if (nilai > tertinggi)
+                    tertinggi = nilai;
+                if (nilai < terendah)
+                    terendah = nilai;
+
+                if (nilai >= 60)
+                    lulus++;
+                else
+                    tidakLulus++;
+            } catch (NumberFormatException e) {
+                System.out.println("Input tidak valid, ulangi mahasiswa ke-" + i);
+                i--; 
+            }
         }
-        System.out.println("Nilai tertinggi: " + tertinggi);
+
+        System.out.println("\nNilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        
+        sc.close();
     }
 }
