88dfb4b03c5a0f7b4fa84466770ffce0a18a6c28
gbyn <byank2314@gmail.com>
2025-10-26 18:58:08 +0700

    latihan1

--- jobsheet7/SiakadFor18.java
+++ jobsheet7/SiakadFor18.java
@@ -2,7 +2,7 @@ package jobsheet7;
 import java.util.Scanner;
 
 public class SiakadFor18 {
-    public static void main(String[] args) {//
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
         for (int i = 1; i <= 10; i++) {
